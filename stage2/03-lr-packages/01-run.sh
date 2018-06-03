6db8500b4c6a438b6472c6ae4fe57e287b2db755
Mr Spoon <1.neo.travis@gmail.com>
2018-06-02 19:19:09 -0500

    Release Candidate 1

--- stage2/03-lr-packages/01-run.sh
+++ stage2/03-lr-packages/01-run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash -e
 
+pip install --root="${ROOTFS_DIR}" requests
 pip install --root="${ROOTFS_DIR}" socketIO-client
+pip3 install --root="${ROOTFS_DIR}" requests
 pip3 install --root="${ROOTFS_DIR}" socketIO-client
+
+git clone "https://github.com/runmyrobot/runmyrobot" "${ROOTFS_DIR}/home/robocaster/runmyrobot"
+chown -Rv 1000:1000 "${ROOTFS_DIR}/home/robocaster/runmyrobot"
+
+install -v -o 1000 -g 1000 -m 644 files/.bash_aliases "${ROOTFS_DIR}/home/robocaster"
+install -v -m 755 files/ctemp	"${ROOTFS_DIR}/usr/local/bin"
+install -v -m 755 files/devices	"${ROOTFS_DIR}/usr/local/bin"
+install -v -m 755 files/00-lr-motd	"${ROOTFS_DIR}/etc/update-motd.d"
+
+echo "i2c-dev" >> ${ROOTFS_DIR}/etc/modules
+
+on_chroot << EOF
+update-command-not-found
+EOF
